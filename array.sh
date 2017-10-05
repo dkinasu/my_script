102be8116516b91ec3bf4c4a52af3d457000820d
dkinasu <mdeng17@asu.edu>
2017-10-05 09:39:34 -0700

    create array.sh

---
+++ array.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# array
+
+array=(niaho ege geg 2 ge 23 4455 322 55 wwwe dmz zegeeq ing)
+echo "Size of the array is: ${#array[*]}"
+echo "Enter the starting index of the array to print out: (starts from 0)"
+read start
+
+
+if [[ start -lt 0]] || [[ start -ge ${#array[*]} ]]
+then
+	echo "Error: input < 0 or input >= size of the array. Plz re-enter"
+	read start
+else
+	echo "Printing out:"
+	echo ${array[*]:$start:}
+
+
